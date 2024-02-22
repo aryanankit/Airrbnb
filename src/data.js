83b7d267cc7a498d29eab627c45d2f033afc4369
aryanankit <a.arya8279@gmail.com>
2024-02-22 21:37:06 +0530

    adjusted the data

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const data= [
         title: "Life  with Katie ",
         description: "I will share  you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "katie-zaferes.png",
+        coverImg: "katie-zafes.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
